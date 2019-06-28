dc7478b85999b348db7df562e3701e940514ee65
Mikhail Komarov <nemo1369@gmail.com>
2019-06-28 03:09:37 +0300

    Install script updated.

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo apt-get install cmake -y
 sudo apt-get install libgmp3-dev -y
+sudo apt-get install libmpir-dev -y
 sudo apt-get install libflint-dev -y
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=TRUE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_NO_BOOST=TRUE -DCMAKE_BUILD_TYPE=Release ..
+cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_GMP=TRUE -DCMAKE_BUILD_TYPE=Release ..
 make -j$(nproc) chia_vdf_test
