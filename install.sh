e053081fc3badc0e94db68da85521e40bb35927a
Mikhail Komarov <nemo1369@gmail.com>
2019-06-28 19:34:20 +0300

    Debian packages installation fixed.

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# sudo apt-get install cmake -y
-# sudo apt-get install libgmp3-dev -y
-# sudo apt-get install libmpfr-dev -y
-# sudo apt-get install libflint-dev -y
+sudo apt-get install cmake -y
+sudo apt-get install libgmp3-dev -y
+sudo apt-get install libmpfr-dev -y
+sudo apt-get install libflint-dev -y
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_GMP=TRUE -DCMAKE_BUILD_TYPE=Release ..
