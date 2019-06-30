14dbe4f1375a6c1b5ec1e07687447734bcf899f4
Mikhail Komarov <nemo1369@gmail.com>
2019-07-01 02:39:55 +0300

    Installation script updated.

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
+sudo apt-get update -y
+sudo apt-get upgrade -y
 sudo apt-get install cmake -y
 sudo apt-get install git -y
 sudo apt-get install yasm -y
 sudo apt-get install m4 -y
 sudo apt-get install build-essential -y
+sudo apt-get install autoconf -y
 sudo apt-get install libgmp3-dev -y
 sudo apt-get install libmpfr-dev -y
 sudo apt-get install libflint-dev -y
+sudo apt-get autoremove
 mkdir build
 cd build
 git clone git://github.com/wbhart/mpir.git mpir
@@ -16,6 +20,7 @@ cd mpir
 make
 make check
 sudo make install
+cd ../
 mkdir release
 cd release
 cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_MPIR=TRUE -DCMAKE_BUILD_TYPE=Release ../../
