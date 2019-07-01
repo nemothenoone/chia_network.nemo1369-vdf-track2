ded2854870beb82d949d3bf1fb9959cf0ef4fd71
Mikhail Komarov <nemo1369@gmail.com>
2019-07-01 13:28:23 +0300

    Installation script rollback.

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@ sudo apt-get install cmake git yasm m4 build-essential autogen automake autoconf
 sudo apt-get install libgmp3-dev libmpfr-dev libflint-dev -y
 mkdir build
 cd build
-git clone git://github.com/wbhart/mpir.git mpir
-cd mpir
-./autogen.sh
-./configure --enable-cxx
-make -j$(nproc) all
-make -j$(nproc) check
-sudo make -j$(nproc) install
-cd ../
+#git clone git://github.com/wbhart/mpir.git mpir
+#cd mpir
+#./autogen.sh
+#./configure --enable-cxx
+#make -j$(nproc) all
+#make -j$(nproc) check
+#sudo make -j$(nproc) install
+#cd ../
 mkdir release
 cd release
-cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_MPIR=TRUE -DCMAKE_BUILD_TYPE=Release ../../
+cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_GMP=TRUE -DCMAKE_BUILD_TYPE=Release ../../
 make chia_vdf_test
