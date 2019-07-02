568dd21990924ab5a1c1dd043d2f741e91a6ba58
Mikhail Komarov <nemo1369@gmail.com>
2019-07-03 02:45:21 +0300

    MPIR explicit check removed from installation script.

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ cd mpir
 ./autogen.sh > /dev/null 2>&1
 ./configure --enable-cxx > /dev/null 2>&1
 make -j$(nproc) > /dev/null 2>&1
-make -j$(nproc) check
+#make -j$(nproc) check
 sudo make -j$(nproc) uninstall > /dev/null 2>&1
 sudo make -j$(nproc) install > /dev/null 2>&1
 cd ../
