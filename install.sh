b28e3fa09ac84f9aed720cc37f585ea50d91f008
Mikhail Komarov <nemo1369@gmail.com>
2019-07-02 20:54:43 +0300

    Installation updated.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get install cmake git yasm m4 build-essential autogen automake autoconf libtool autotools-dev texinfo -y
+sudo apt-get install libmpfr-dev -y
 #sudo apt-get install libmpfr-dev libflint-dev -y
 mkdir build
 cd build
@@ -13,12 +14,12 @@ cd build
 #sudo apt-get install libgmp3-dev -y
 
 sudo apt-get purge libgmp3-dev libmpfr-dev libflint-dev -y
-git clone git://github.com/wbhart/mpir.git
-cd mpir
-#wget http://mpir.org/mpir-3.0.0.tar.bz2
-#tar xvfj mpir-3.0.0.tar.bz2
-#cd mpir-3.0.0
-./autogen.sh
+#git clone git://github.com/wbhart/mpir.git
+#cd mpir
+wget http://mpir.org/mpir-3.0.0.tar.bz2
+tar xvfj mpir-3.0.0.tar.bz2
+cd mpir-3.0.0
+#./autogen.sh
 ./configure --enable-cxx
 make -j$(nproc) all
 make -j$(nproc) check
