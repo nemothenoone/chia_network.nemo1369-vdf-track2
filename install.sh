a5e3d825c683c487277b76ec7635a69905ed7de7
Mikhail Komarov <nemo1369@gmail.com>
2019-07-02 17:39:00 +0300

    Installation script updated.

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 sudo apt-get install cmake git yasm m4 build-essential autogen automake autoconf libtool -y
+sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmpxx.a
+sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmp.a
 sudo apt-get install libgmp3-dev libmpfr-dev libflint-dev -y
 mkdir build
 cd build
-#git clone git://github.com/wbhart/mpir.git mpir
-#cd mpir
-#./autogen.sh
-#./configure --enable-cxx
-#make -j$(nproc) all
-#make -j$(nproc) check
-#sudo make -j$(nproc) install
-#cd ../
 mkdir release
 cd release
 cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_GMP=TRUE -DCMAKE_BUILD_TYPE=Release ../../
