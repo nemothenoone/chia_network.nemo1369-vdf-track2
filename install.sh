b35450d34608762f3b7765fed840e7f3f1083f69
Mikhail Komarov <nemo1369@gmail.com>
2019-07-02 21:36:46 +0300

    More installation updates..

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@ sudo apt-get install libmpfr-dev libflint-dev libgmp3-dev -y
 mkdir build
 cd build
 
-#wget http://mpir.org/mpir-3.0.0.tar.bz2
-#tar xvfj mpir-3.0.0.tar.bz2
-#cd mpir-3.0.0
-#./configure --enable-cxx
-#sudo make -j$(nproc) uninstall
+wget http://mpir.org/mpir-3.0.0.tar.bz2
+tar xvfj mpir-3.0.0.tar.bz2
+cd mpir-3.0.0
+./configure --enable-cxx
+sudo make -j$(nproc) uninstall
 #cd ../
 
 #git clone git://github.com/wbhart/mpir.git
 #cd mpir
-wget http://mpir.org/mpir-3.0.0.tar.bz2
-tar xvfj mpir-3.0.0.tar.bz2
-cd mpir-3.0.0
+#wget http://mpir.org/mpir-3.0.0.tar.bz2
+#tar xvfj mpir-3.0.0.tar.bz2
+#cd mpir-3.0.0
 #./autogen.sh
-./configure --enable-cxx
+#./configure --enable-cxx
 make -j$(nproc) all
-make -j$(nproc) check
-sudo make -j$(nproc) uninstall
-sudo make -j$(nproc) install
+#make -j$(nproc) check
+#sudo make -j$(nproc) uninstall
+#sudo make -j$(nproc) install
 cd ../
 
 mkdir release
