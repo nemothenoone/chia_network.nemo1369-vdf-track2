49c1ea6cbda9d5629d1591fda036f365a37e7f3e
Mikhail Komarov <nemo1369@gmail.com>
2019-07-03 02:30:32 +0300

    More installation script silenced.

--- install.sh
+++ install.sh
@@ -26,9 +26,9 @@ cd mpir
 ./autogen.sh > /dev/null 2>&1
 ./configure --enable-cxx > /dev/null 2>&1
 make -j$(nproc) > /dev/null 2>&1
-#make -j$(nproc) check
-sudo make -j$(nproc) uninstall
-sudo make -j$(nproc) install
+make -j$(nproc) check
+sudo make -j$(nproc) uninstall > /dev/null 2>&1
+sudo make -j$(nproc) install > /dev/null 2>&1
 cd ../
 
 mkdir release
