b8c7056fb8f6ad0a727983a120b751cbd1a3ebc7
Mikhail Komarov <nemo1369@gmail.com>
2019-07-03 00:38:08 +0300

    Installation updated.

--- install.sh
+++ install.sh
@@ -22,9 +22,9 @@ cd mpir
 #wget http://mpir.org/mpir-3.0.0.tar.bz2
 #tar xvfj mpir-3.0.0.tar.bz2
 #cd mpir-3.0.0
-./autogen.sh > log
-./autogen.sh > log
-./configure
+./autogen.sh &> log
+./autogen.sh &> log
+./configure --enable-cxx
 make -j$(nproc) all
 #make -j$(nproc) check
 #cd tune
