585805537444683588b3e370ff3c88a5efbae532
Mikhail Komarov <nemo1369@gmail.com>
2019-07-03 03:54:36 +0300

    Temporary rollback.

--- install.sh
+++ install.sh
@@ -17,21 +17,21 @@ cd build
 #sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmpxx.a
 #sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmp.a
 
-git clone git://github.com/wbhart/mpir.git > /dev/null 2>&1
-cd mpir
+#git clone git://github.com/wbhart/mpir.git > /dev/null 2>&1
+#cd mpir
 #wget http://mpir.org/mpir-3.0.0.tar.bz2
 #tar xvfj mpir-3.0.0.tar.bz2
 #cd mpir-3.0.0
-./autogen.sh > /dev/null 2>&1
-./autogen.sh > /dev/null 2>&1
-./configure --enable-cxx --build=skylake-pc-linux-gnu > /dev/null 2>&1
-make -j$(nproc) > /dev/null 2>&1
-make -j$(nproc) check
-sudo make -j$(nproc) uninstall > /dev/null 2>&1
-sudo make -j$(nproc) install > /dev/null 2>&1
-cd ../
+#./autogen.sh > /dev/null 2>&1
+#./autogen.sh > /dev/null 2>&1
+#./configure --enable-cxx --build=skylake-pc-linux-gnu > /dev/null 2>&1
+#make -j$(nproc) > /dev/null 2>&1
+#make -j$(nproc) check
+#sudo make -j$(nproc) uninstall > /dev/null 2>&1
+#sudo make -j$(nproc) install > /dev/null 2>&1
+#cd ../
 
 mkdir release
 cd release
-cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_MPIR=TRUE -DCMAKE_BUILD_TYPE=Release ../../
+cmake -DBUILD_SHARED_LIBS=FALSE -DBUILD_UNIT_TESTS=FALSE -DCRYPTO3_VDF_BOOST=FALSE -DCRYPTO3_VDF_GMP=TRUE -DCMAKE_BUILD_TYPE=Release ../../
 make chia_vdf_test
